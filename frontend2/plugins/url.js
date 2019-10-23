74b3c3e0cf436780e2a25892356234d7a55300d4
zardilior <jeestremadoyro@gmail.com>
2019-10-23 08:07:33 -0500

    cambios

--- frontend2/plugins/url.js
+++ frontend2/plugins/url.js
@@ -1,5 +1,5 @@
 export default ({ app, env }, inject) => {
-  // const url = 'https://proyecto-integrador-red-social.appspot.com'
-  const url = 'http://localhost:4000'
+  const url = 'http://backend-dot-proyecto-integrador-red-social.appspot.com'
+  // const url = 'http://localhost:4000'
   return inject('url', url)
 }
