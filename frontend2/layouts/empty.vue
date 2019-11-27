1980f46726875385561636a160a38257bbec244e
zardilior <jeestremadoyro@gmail.com>
2019-11-27 08:06:31 -0600

    changes in style register

--- frontend2/layouts/empty.vue
+++ frontend2/layouts/empty.vue
@@ -8,4 +8,11 @@ export default {
 }
 </script>
 <style scoped>
+.theme--dark.v-application {
+  background-color: #263238;
+}
+::v-deep .theme--dark.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+  color: #263238;
+  background-color: #acdff9;
+}
 </style>
