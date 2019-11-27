e99fdc94455348a7ecdb3b0bc334c19846d9a706
Manuel Alejandro Hernández Peña <manuelitowii@live.com.mx>
2019-11-26 23:41:36 -0600

    Style changes

---
+++ create.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-header class="background">
+        <h4> Create Favor</h4>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content class="background">
+        <v-form
+          v-model="valid"
+          class="pa-4"
+          @submit="submit"
+        >
+          <v-text-field
+            v-model="favor.title"
+            label="Title"
+            placeholder="This is what people will first see!"
+            class="field"
+          />
+          <v-text-field
+            v-model="favor.coins"
+            label="Coins"
+            placeholder="0"
+            prefix="$"
+            suffix=".00"
+            class="field"
+          />
+          <date-picker
+            v-model="favor.deadline"
+            label="Deadline"
+            placeholder="DD/MM/YY"
+            class="field"
+          />
+          <v-textarea
+            v-model="favor.description"
+            label="Description"
+            placeholder="Tell us more details! Acceptance criteria, the story behind it"
+            class="field"
+          />
+          <v-btn :disabled="!valid" type="submit">
+            Submit
+          </v-btn>
+        </v-form>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+<script>
+export default {
+  components: {
+    DatePicker: () => import('@/components/ui/date-picker')
+  },
+  data () {
+    return {
+      valid: false,
+      favor: {},
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => v ? v.length >= 8 || 'Min 8 characters' : false,
+        email: v => /.+@.+\..+/.test(v) || 'Invalid Email address'
+      }
+    }
+  },
+  methods: {
+    submit (e) {
+      e.preventDefault()
+      this.$emit('submit', Object.assign({}, this.favor))
+      this.favor = {}
+    }
+  }
+}
+</script>
+<style scoped>
+.field{
+  background-color: #4B4B4B;
+  border-radius: 5px;
+  padding: 5%;
+  margin-bottom: 7.5%;
+}
+.background{
+  background-color: #B3B3B3;
+  color: black;
+}
+</style>
---
+++ list.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-card class="border">
+    <v-card-title>
+      <slot name="title"/>
+    </v-card-title>
+    <v-card-text>
+      <v-list>
+        <v-list-item
+          v-for="(favor,index) in favors"
+          :key="index"
+        >
+          <v-card class="pa-4 mt-4 background" width="100%">
+            <v-card-title class="text-capitalize">
+              <h3> {{ favor.title }} </h3> &nbsp;
+            </v-card-title>
+            <v-card-text>
+              <p class="text"> {{ favor.description }}</p>
+              <small class="text-left text">
+                <div>
+                  {{ favor.creator }} needs it before {{ favor.deadline }}
+               </div>
+              </small>
+               <div class="text">
+                  ${{ favor.coins }} coins
+                </div>
+            </v-card-text>
+          </v-card>
+        </v-list-item>
+        <v-list-item v-if="favors.length===0">
+          {{ emptyMessage }}
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    favors: {
+      type: Array,
+      default: Array
+    },
+    emptyMessage: {
+      type: String,
+      default: () => 'No favors yet'
+    }
+  }
+}
+</script>
+<style scoped>
+.field{
+  background-color: #4B4B4B;
+  border-radius: 5px;
+  padding: 5%;
+  margin-bottom: 7.5%;
+}
+.background{
+  background-color: #B3B3B3;
+  color: black;
+}
+.text{
+  color: black;
+}
+.border{
+  border-style: solid;
+  border-color: #5F5F5F;
+  border-width: 5px;
+}
+</style>
