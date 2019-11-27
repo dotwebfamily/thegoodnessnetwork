cdbf45176ae632749f0483dd91b85859c9305a9c
Manuel Alejandro Hernández Peña <manuelitowii@live.com.mx>
2019-11-26 23:42:57 -0600

    Style changes

--- frontend2/components/favors/list.vue
+++ frontend2/components/favors/list.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card>
+  <v-card class="border">
     <v-card-title>
       <slot name="title"/>
     </v-card-title>
@@ -9,18 +9,20 @@
           v-for="(favor,index) in favors"
           :key="index"
         >
-          <v-card class="pa-4 mt-4" width="100%">
+          <v-card class="pa-4 mt-4 background" width="100%">
             <v-card-title class="text-capitalize">
               <h3> {{ favor.title }} </h3> &nbsp;
             </v-card-title>
-            <v-card-subtitle>
-              <h5> ${{ favor.coins }} coins</h5>
-            </v-card-subtitle>
             <v-card-text>
-              <p> {{ favor.description }}</p>
-              <small class="text-right">
-                {{ favor.creator }} needs it before {{ favor.deadline }}
+              <p class="text"> {{ favor.description }}</p>
+              <small class="text-left text">
+                <div>
+                  {{ favor.creator }} needs it before {{ favor.deadline }}
+               </div>
               </small>
+               <div class="text">
+                  ${{ favor.coins }} coins
+                </div>
             </v-card-text>
           </v-card>
         </v-list-item>
@@ -46,4 +48,22 @@ export default {
 }
 </script>
 <style scoped>
+.field{
+  background-color: #4B4B4B;
+  border-radius: 5px;
+  padding: 5%;
+  margin-bottom: 7.5%;
+}
+.background{
+  background-color: #B3B3B3;
+  color: black;
+}
+.text{
+  color: black;
+}
+.border{
+  border-style: solid;
+  border-color: #5F5F5F;
+  border-width: 5px;
+}
 </style>
